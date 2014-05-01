922a80e8441ad3a2ec7a69a1c9b07e663dbdbbcf
Antonio Augusto <antonioabritto@gmail.com>
2014-04-30 21:39:48 -0300

    first and second iterations, setup and first model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Timetracker::Application.config.secret_key_base = '33086cc4611cb43928d0b03c591601cfd9767960e6acd3133c7a4f7cc2b00aa9647f92b58b8c76bf16ffa1ad94f24e0ca95003d08379d9d05160b4ea8262cdb5'
---
+++ spec/models/account_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe Account do
+  describe "Validations" do
+    it { should validate_presence_of :subdomain }
+    it { should validate_uniqueness_of :subdomain }
+
+    it { should allow_value('xtrdev').for(:subdomain) }
+    it { should allow_value('test').for(:subdomain) }
+
+    it { should_not allow_value('www').for(:subdomain) }
+    it { should_not allow_value('WWW').for(:subdomain) }
+    it { should_not allow_value('.test').for(:subdomain) }
+    it { should_not allow_value('test/').for(:subdomain) }
+
+    it "should validate case insensitive uniqueness" do
+      create(:account, subdomain: 'Test')
+      expect(build(:account, subdomain: 'test')).to_not be_valid
+    end
+  end
+
+  describe "Associations" do
+    it "should have owner"
+  end
+end
